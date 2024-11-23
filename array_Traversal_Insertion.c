c47dcdbb76c08551bedd808c5cb11bbd0ad75f41
Shivam <sm2552744@gmail.com>
2024-11-23 17:09:38 +0530

    file_1

--- array_Traversal_Insertion.c
+++ array_Traversal_Insertion.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 
-int main(){
+void arrayTraversalInsertion();
 
+
+
+int main(){
+    
+    arrayTraversalInsertion();
+ 
+    return 0;
 }
 
 
 
 void arrayTraversalInsertion(){
-    int 
+    int val, i;
+    int arr[50];
+
+    printf("Enter size of Array - ");
+    scanf("%d",&val);
+
+    for(i = 0; i < val; i++){
+        printf("Enter %d values - ",i);
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Here is Traversing Result of Array - ");
+    for(i = 0; i < val; i++){
+        (i < val - 1) ? printf("%d, ",arr[i]) : printf("%d",arr[i]) ;
+    }
+    
 }
