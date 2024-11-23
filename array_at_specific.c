94f006be18c0c2e8c0c2ecf4b6bfd8478762e0d9
Shivam <sm2552744@gmail.com>
2024-11-23 19:44:13 +0530

    file_2

--- array_at_specific.c
+++ array_at_specific.c
@@ -52,8 +52,6 @@ int main(){
 
 
         printf("\nArray after insertion - \n");
-        
-
         for(int i = 0; i < size; i++){
             (i < size - 1) ? printf("%d, ",arr[i]) : printf("%d",arr[i]) ;
         }
