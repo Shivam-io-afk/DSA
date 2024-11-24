32e92b0c86c34b323922dab6eea40634c9ed797f
Shivam <sm2552744@gmail.com>
2024-11-24 22:30:53 +0530

    file_4

--- ArrayPointer.c
+++ ArrayPointer.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
 int main(){
-    
+    int arr[50];
+    int *p;
+    p = arr;
+
+    printf("Enter - ");
+    for(int i = 0; i < 5; i++){
+        // scanf("%d",(arr+i));
+        scanf("%d",(p+i));
+        // scanf("%d",(i+arr));
+        // scanf("%d",&p[i]);
+    }
+
+
+
+    for(int i = 0; i < 5; i++){
+        // printf("%d, ",arr[i]);
+        // printf("%d ",*(p+i));
+        // printf("%d ",i[arr]);
+        printf("%d ",i[p]);
+    }
 }
