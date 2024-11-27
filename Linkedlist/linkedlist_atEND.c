4711212d0bb5bddf95d526a6b4d5d1abd56fa25b
Shivam <sm2552744@gmail.com>
2024-11-27 21:51:27 +0530

    linkedlist

--- Linkedlist/linkedlist_atEND.c
+++ Linkedlist/linkedlist_atEND.c
@@ -1,18 +1,18 @@
-#include <stdio>
+#include <stdio.h>
 #include <stdlib.h>
 
 
 struct node {
     int data;
     struct node* next;
-}
+};
 
 
 struct node* createNode(int data){
     struct node* newNode = (struct node*)malloc(sizeof(struct node));
 
     if(newNode == NULL){
-        prinf("Memory not allocated");
+        printf("Memory not allocated");
         exit(0);
     }
 
@@ -42,14 +42,39 @@ void insertAtEND(struct node** head,int data){
 int printList(struct node* head){
     struct node* temp = head;
     if(temp != NULL){
-        
+        printf("%d", temp->data);
+        temp = temp -> next;
     }
+
+    printf("-> NULL\n");
+
+
 }
 int main(){
-    struct node *temp = NULL;
-    insertAtEND(&temp,30);
+    struct node *head = NULL;
+    int choice;
+    printf("Insertion in linked list ->");
+    
+    while(1 != 0){
+        printf("\nEnter what you want to do -> \n");
+        printf("1> To insert at end \n");
+        printf("2> To Display \n");
 
-
-
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                int val;
+                printf("Insert your value - ");
+                scanf("%d",&val);
+                insertAtEND(&head, val);
+                break;
+            case 2:
+                printf("List - \n");
+                printList(head);
+                break;
+            default:
+                exit(0);
+        }
+    }
     return 0;
 }
