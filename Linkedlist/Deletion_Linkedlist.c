b63b871a5a26df5c037c5f7fbdff77fbd939dbf8
Shivam <sm2552744@gmail.com>
2024-12-01 09:42:57 +0530

    linkedlist

--- Linkedlist/Deletion_Linkedlist.c
+++ Linkedlist/Deletion_Linkedlist.c
@@ -8,3 +8,23 @@ struct node {
 }
 
 
+void createNode(struct node* head,int data){
+    struct node* newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+
+void insert(struct node** head, int data){
+    struct node** newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = *head;
+    *head = newNode;
+}
+
+
+void delete(struct node** head, int data){
+    
+
+}
