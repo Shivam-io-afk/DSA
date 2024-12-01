bc3c79acd71a11315f9e860bb55941ad4f53b820
Shivam <sm2552744@gmail.com>
2024-12-01 18:55:35 +0530

    linekdlist

--- Linkedlist/insertion_at_index.c
+++ Linkedlist/insertion_at_index.c
@@ -8,15 +8,15 @@ struct node{
 };
 
 
-struct node* createNode(struct node* head){
+struct node* createNode(int data){
     struct node* newNode = (struct node *)malloc(sizeof(struct node));
     if(newNode == NULL){
         printf("Memory Allocation Failed");
-        return;
+        return 0;
     }
 
-    newNode->data = head;
-    newNode->next = NULL:
+    newNode->data = data;
+    newNode->next = NULL;
 
     return newNode;
 
@@ -24,11 +24,65 @@ struct node* createNode(struct node* head){
 
 
 void inserting(struct node** head, int data){
-    struct node* newNode = (struct node *)malloc(sizeof(struct node));
-
+    struct node* newNode = createNode(data);
     newNode->next = *head;
     *head = newNode;
-    
-
 }
 
+
+
+//--------------------------------------------->
+void insertAtIndex(struct node** head, int index, int value) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (index == 0) {
+        newNode->next = *head;
+        *head = newNode;
+        return;
+    }
+
+    struct node* temp = *head;
+    int count = 0;
+    while (temp != NULL && count < index - 1) {
+        temp = temp->next;
+        count++;
+    }
+
+    if (temp == NULL) {
+        printf("Index out of bounds.\n");
+        free(newNode);
+        return;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+//--------------------------------------------->
+
+
+
+void printing(struct node *head){
+    struct node* temp = head;
+    while(temp != NULL){
+        printf("%d -> ",temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+
+int main(){
+    struct node* head = NULL;
+
+    inserting(&head,65); 
+    inserting(&head,42); 
+    inserting(&head,89); 
+
+    printing(head);
+
+
+    insertAtIndex(&head, 3, 12);
+    printing(head);
+}
