ae10ba41575dc0031b3e789b9c3e349c87f6bf09
Shivam <sm2552744@gmail.com>
2024-12-02 10:25:58 +0530

    typecasting

---
+++ typecasting.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+// Basic program to perform basic arithmetic operations - 
+int main(){
+    // tpyecasting
+    // (type) value;
+
+
+    int a = 100;
+    float b = 260/10;
+
+
+    printf("%d\n",a);
+
+    printf("%f\n",b);//typecasting this ->
+    printf("%d\n",(int)b);
+    
+    printf("%f\n",a);//typecasting this ->
+    printf("%f\n",(float)a);
+
+}
